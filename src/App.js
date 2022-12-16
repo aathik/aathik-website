3274e25f08e37de877a316cfe73cdfe40bb9012e
aathik tr <aathiktr@gmail.com>
2022-12-17 04:08:55 +0530

    Deploy2

--- src/App.js
+++ src/App.js
@@ -7,12 +7,18 @@ import Hero from './Components/Hero';
 import Navbar from './Components/Navbar';
 import Projects from './Components/Projects';
 import Skills from './Components/Skills';
+import {Helmet} from "react-helmet";
 
 
 
 const App = () => {
   return (
     <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Aathik</title>
+        <link rel="canonical" href="https://aathik.github.io/aathik-website/" />
+      </Helmet>
       <Navbar />
       <Hero />
       <About />
